3f5e411e24c553e628287f1e8b1f0c37f5a0d904
Clement Scheirlinck <clement.scheirlinck@etu.univ-lyon1.fr>
2019-09-11 20:38:16 +0200

    create controller classes for mvc pattern

---
+++ cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/controller/Controller.java
@@ -0,0 +1,36 @@
+package fr.univ_lyon1.info.m1.cv_search.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+
+    private List<Component> skills;
+    private List<Component> filters;
+
+    Controller(){
+
+    }
+
+    public List<String> receiveRequest(Request req){
+        skills = new ArrayList<Component>();
+        filters = new ArrayList<Component>();
+
+        for (Component c: req.getComponents()) {
+            if(c.isSkill()){
+                skills.add(c);
+            }
+            else if(c.isFilter()){
+                filters.add(c);
+            }
+        }
+        return this.executeRequest();
+    }
+
+    private List<String> executeRequest(){
+        List<String> result = new ArrayList<String>();
+        // TODO: make it
+        return result;
+    }
+
+}
