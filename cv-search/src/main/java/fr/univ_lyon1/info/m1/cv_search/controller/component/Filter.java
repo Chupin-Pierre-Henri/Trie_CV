5463c4bc17f5814fbbd60d89779ba77a77057484
Clement Scheirlinck <clement.scheirlinck@etu.univ-lyon1.fr>
2019-09-17 11:39:21 +0200

    add getter and change package

--- cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/controller/component/Filter.java
+++ cv-search/src/main/java/fr/univ_lyon1/info/m1/cv_search/controller/component/Filter.java
@@ -1,4 +1,4 @@
-package fr.univ_lyon1.info.m1.cv_search.view;
+package fr.univ_lyon1.info.m1.cv_search.controller.component;
 
 public class Filter implements Component {
     private String name;
@@ -19,4 +19,11 @@ public class Filter implements Component {
         return true;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
